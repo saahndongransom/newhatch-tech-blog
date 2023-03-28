a015022a2600ab34ec492e8ccb10dc33fad1f0bd
Saahndong <saahndongransom>
2023-03-28 19:09:27 +0100

    blog

--- nblog/admin.py
+++ nblog/admin.py
@@ -1,8 +1,19 @@
-
+#from .models import Post,Tag
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment
 from .models import  SubscribedUsers
-from . models import CustomUser, Comment
+from .models import Category
+
+
+from django.forms.widgets import TextInput
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
+
+
+
+#from.models import Comment
+
 
 
 # Customize the way the admin panel looks
@@ -17,12 +28,40 @@ class SubscribedUsersAdmin(admin.ModelAdmin):
 
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_date', 'post', 'active')
+    list_filter = ('active', 'created_date')
+    search_fields = ('name', 'email', 'text')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+class CKEditorWidget(TextInput):
+    template_name = 'admin/ckeditor_widgets'
+
+
+
+
+
 
 
 
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
+admin.site.register(SubscribedUsers, SubscribedUsersAdmin,)
+
+admin.site.register(Category)
+
+#admin.site.register([Tag])
 admin.site.register(Comment)
-admin.site.register(SubscribedUsers, SubscribedUsersAdmin)
-admin.site.register(CustomUser)
+#admin.site.register (tags)
+#admin.site.register(Tag)
+
+
+
+
+
+
+
